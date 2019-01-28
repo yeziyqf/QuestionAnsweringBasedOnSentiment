d88ed50f050dacc504032f9b26527d822c7fafc6
Ye Qiaofei <511198980@qq.com>
2019-01-28 10:58:36 -0500

    NLTK take file as input version.

--- sentimentAnalyzer_NLTK.py
+++ sentimentAnalyzer_NLTK.py
@@ -12,55 +12,38 @@ nltk.download('vader_lexicon')
 file = pd.read_csv('C:\SentStrength_Data\data\question_tab_withquote_noSpace.txt',
             delimiter=',', encoding='utf-8', names=['question'])
 print(file)
-file.iloc[:, 0].tolist()
-
-
-sentences = ["I want to lose weight.",  # positive sentence example
-             "VADER is smart, handsome, and funny!",
-             # punctuation emphasis handled correctly (sentiment intensity adjusted)
-             "VADER is very smart, handsome, and funny.",
-             # booster words handled correctly (sentiment intensity adjusted)
-             "VADER is VERY SMART, handsome, and FUNNY.",  # emphasis for ALLCAPS handled
-             "VADER is VERY SMART, handsome, and FUNNY!!!",
-             # combination of signals - VADER appropriately adjusts intensity
-             "VADER is VERY SMART, really handsome, and INCREDIBLY FUNNY!!!",
-             # booster words & punctuation make this close to ceiling for score
-             "The book was good.",  # positive sentence
-             "The book was kind of good.",  # qualified positive sentence is handled correctly (intensity adjusted)
-             "The plot was good, but the characters are uncompelling and the dialog is not great.",
-             # mixed negation sentence
-             "A really bad, horrible book.",  # negative sentence with booster words
-             "At least it isn't a horrible book.",  # negated negative sentence with contraction
-             ":) and :D",  # emoticons handled
-             "",  # an empty string is correctly handled
-             "Today sux",  # negative slang handled
-             "Today sux!",  # negative slang with punctuation emphasis handled
-             "Today SUX!",  # negative slang with capitalization emphasis
-             "Today kinda sux! But I'll get by, lol"
-             # mixed sentiment example with slang and constrastive conjunction "but"
-             ]
+sentences = file.iloc[:, 0].tolist()
+# print(sentences)
+# print(len(sentences))
 
 sid = SentimentIntensityAnalyzer()
 total = 0
 positive = 0
 negative = 0
 significance = 0
-threshold = 0.1
+threshold = 0.4
+df = pd.DataFrame([])
 for sentence in sentences:
     ss = sid.polarity_scores(sentence)
     # print(ss['compound'], ss['neg'], ss['neu'], ss['pos'])
     if (ss['compound'] > threshold) or (ss['compound'] < -1 * threshold):
-        print(sentence)
-        for k in sorted(ss):
-            print('{0}: {1}, '.format(k, ss[k]), end='')
-        print()
-        print()
+        # print(sentence)
+        df_new = pd.DataFrame([[sentence]])
+        df = pd.concat([df, df_new])
+        # for k in sorted(ss):
+        #     print('{0}: {1}, '.format(k, ss[k]), end='')
+        # print()
+        # print()
         significance += 1
         if ss['compound'] > 0.1:
-            positive +=1
+            positive += 1
         if ss['compound'] < -0.1:
             negative += 1
     total += 1
+
+print(df)
+print(df_new)
+# df.to_csv("C:\SentStrength_Data\data\Report\Intersection data\selectedQuestion_NLTK.txt", index=False)
 
 print('total: ' + str(total))
 print('significance: ' + str(significance))
